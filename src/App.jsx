d2bcf9553f27439583608b36112a3bf9a6622dc5
lujanmaccari <mlujanmaccari@gmail.com>
2025-05-26 20:19:09 -0300

    se agrega bootstrap al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Card } from "react-bootstrap";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Health tracker</h1>
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>Health Tracker</Card.Title>
+          <Card.Text>IFTS18.</Card.Text>
+        </Card.Body>
+      </Card>
     </>
   );
 }
