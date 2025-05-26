d34c217f3e94475a5f961a15ab2918dee7926740
lujanmaccari <mlujanmaccari@gmail.com>
2025-05-26 20:59:58 -0300

    se agrega SignIn

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-
-
-import { Routes, Route } from "react-router-dom";
-import Home from "./screens/Home";
-
-import GoalsView from "./screens/GoalsView";
+import { Route, Routes } from "react-router-dom";
 import CategoryView from "./screens/CategoryView";
-import UserView from "./screens/User";
+import GoalsView from "./screens/GoalsView";
 import SignIn from "./screens/SignIn";
-// import Login from "./screens/Login";
+import UserView from "./screens/User";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<SignIn />} />
       <Route path="/goals" element={<GoalsView />} />
       <Route path="/categories/:category" element={<CategoryView />} />
       <Route path="/user" element={<UserView />} />
-      <Route path="/sign_in" element={<SignIn />} />
-      {/* <Route path="/login" element={<Login />} /> */}
     </Routes>
   );
 }
--- src/screens/SignIn.jsx
+++ src/screens/SignIn.jsx
@@ -1,1 +1,56 @@
-export default <></>
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+
+const SignIn = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const email = form.elements.dni.value;
+    const password = form.elements.password.value;
+
+    console.log(email, password);
+  };
+
+  return (
+    <div className="fullscreen-center">
+      <Card style={{ width: "22rem" }} className="p-3">
+        <Card.Body>
+          <Card.Title className="mb-3 text-center">Iniciar Sesión</Card.Title>
+
+          {/* {showAlert && (
+            <Alert
+              variant="danger"
+              onClose={() => setShowAlert(false)}
+              dismissible
+            >
+              Credenciales incorrectas
+            </Alert>
+          )} */}
+
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="dni">
+              <Form.Label>DNI</Form.Label>
+              <Form.Control type="number" placeholder="Ingresá tu DNI" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="password">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Ingresá tu contraseña"
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" className="w-100">
+              Entrar
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default SignIn;
